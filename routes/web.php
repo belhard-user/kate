a5c9444e7a51ab044abe763f16ba2a2f896bd5a5
Helene <lenagritsuk@inbox.ru>
2017-06-25 17:34:59 +0300

    route

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hello-page/{name?}', function($name=123){
+    return "Hello " . strtoupper($name);
+})->where([
+    'name' => '\d'
+])->name('foo');
+
+Route::get('url', function(){
+    return route('foo', [
+        'name' => 'Kate',
+        'foo' => 'bar'
+    ]);
+});
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::group(['prefix' => 'dashboard'], function(){
+        Route::get('about', function(){
+            return 'about';
+        });
+    });
+    Route::get('user', function(){ return  'users'; });
+    Route::get('portfolio', function(){ return  'portfolio'; });
+});
